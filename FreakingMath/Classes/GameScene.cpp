3111763b47a18fa89968e79ec03f69cac4aad289
long <longng@nexlesoft.com>
2017-04-04 17:41:25 +0700

    s

--- FreakingMath/Classes/GameScene.cpp
+++ FreakingMath/Classes/GameScene.cpp
@@ -23,7 +23,7 @@ bool GameScene::init()
     Vec2 origin = Director::getInstance()->getVisibleOrigin();
 
     /////////////////////////////
-    this->colorLayer =  LayerColor::create(Color4B(200,0,0,255), visibleSize.width, visibleSize.height);
+    this->colorLayer =  LayerColor::create(this->getRandomColor(), visibleSize.width, visibleSize.height);
     this->colorLayer->setPosition(Vec2(origin.x ,
                                 origin.y));
     this->addChild(colorLayer);
@@ -31,13 +31,13 @@ bool GameScene::init()
     ui::Button* trueItem = ui::Button::create("true.png","true_select.png");
     trueItem->setPosition(Vec2(origin.x + 15 + trueItem->getContentSize().width/2,
                                origin.y + 15 + trueItem->getContentSize().height/2));
-    trueItem->addTouchEventListener( CC_CALLBACK_1(GameScene::menuTrueCallback, this) );
+    trueItem->addTouchEventListener( CC_CALLBACK_2(GameScene::menuTrueCallback, this) );
     this->addChild(trueItem);
     
     ui::Button* falseItem = ui::Button::create("false.png","false_select.png");
     falseItem->setPosition(Vec2(origin.x +  visibleSize.width - falseItem->getContentSize().width/2 - 15 ,
                                 origin.y + 15 + falseItem->getContentSize().height/2));
-    falseItem->addTouchEventListener( CC_CALLBACK_1(GameScene::menuFalseCallback, this) );
+    falseItem->addTouchEventListener( CC_CALLBACK_2(GameScene::menuFalseCallback, this) );
     this->addChild(falseItem);
 
     
@@ -47,63 +47,173 @@ bool GameScene::init()
     // add a label shows "Hello World"
     // create and initialize a label
     
-     this->labelRow1 = Label::createWithTTF("2+2", "fonts/relay-black.ttf", 60);
+     this->labelRow1 = Label::createWithTTF("10+1", "fonts/relay-black.ttf", 60);
     // position the label on the center of the screen
-    labelRow1->setPosition(Vec2(origin.x + visibleSize.width/2,
+    this->labelRow1->setPosition(Vec2(origin.x + visibleSize.width/2,
                             origin.y + visibleSize.height/2 + labelRow1->getContentSize().height));
+    this->row1Pos =  this->labelRow1->getPosition();
 
     // add the label as a child to this layer
     this->addChild(this->labelRow1, 1);
     
     
-    this->labelRow2 = Label::createWithTTF("=6", "fonts/relay-black.ttf", 60);
+    this->labelRow2 = Label::createWithTTF("=12", "fonts/relay-black.ttf", 60);
     // position the label on the center of the screen
-    labelRow2->setPosition(Vec2(origin.x + visibleSize.width/2,
-                                origin.y + visibleSize.height/2 - 0.25*labelRow2->getContentSize().height));
-    
+    this->labelRow2->setPosition(Vec2(origin.x + visibleSize.width/2,
+                                origin.y + visibleSize.height/2 - 0.05*labelRow2->getContentSize().height));
+    this->row2Pos =  this->labelRow2->getPosition();
+
     // add the label as a child to this layer
     this->addChild(this->labelRow2, 1);
 
-   
+    this->newGame();
     
     return true;
 }
 
 void GameScene::newGame()
 {
+    srand(time(NULL));
+    std::string row1;
+    this->numA = this->getRandomNumber() ;
+    this->numB = this->getRandomNumber() ;
+
+    row1 =  std::to_string(this->numA ) + "+" +   std::to_string(this->numB);
+    this->labelRow1->setString(row1.c_str());
     
+    std::string row2;
+    int bigger = this->numA  > this->numB ? this->numA : this->numB ;
+    auto ran =  rand() % 10;
+    if(ran >= 5){
+        this->numResult = this->getRandomResultNumber(bigger);
+    }else{
+        this->numResult = this->getRealResultNumber();
+    }
+    row2  = "=" +  std::to_string(this->numResult)  ;
+    this->labelRow2->setString(row2.c_str());
 }
 
-void GameScene::menuTrueCallback(Ref* pSender)
+void GameScene::nextRound()
+{
+    this->labelRow1->stopAllActions();
+    this->labelRow2->stopAllActions();
+    
+    
+    
+    float timeMove = 0.5;
+    auto moveTo = MoveTo::create(timeMove, Vec2(0, this->row1Pos.y));
+    auto fadeOut = FadeOut::create(timeMove);
+    
+    auto firstSpawn = Spawn::createWithTwoActions(moveTo, fadeOut);
+    
+    auto callbackChangePosition =  CallFunc::create([&, this](){
+        
+        auto visibleSize = Director::getInstance()->getWinSize();
+          CCLOG("reset: %f %f", visibleSize.width, this->labelRow1->getPosition().y);
+        this->labelRow1->setPosition(Vec2(visibleSize.width, this->labelRow1->getPosition().y));
+    });
+     CCLOG("curent: %f %f", this->row1Pos.x, this->row1Pos.y);
+    auto moveTo2 = MoveTo::create(timeMove, Vec2(this->row1Pos.x, this->row1Pos.y));
+    auto fadeIn = FadeIn::create(timeMove);
+    
+    auto secondSpawn = Spawn::createWithTwoActions(moveTo2, fadeIn);
+    
+    auto sequence = Sequence::create(firstSpawn, callbackChangePosition, secondSpawn, nullptr);
+
+    
+    this->labelRow1->runAction(sequence);
+//     this->labelRow1->runAction(moveBy);
+
+}
+
+void GameScene::menuTrueCallback(Ref* pSender, cocos2d::ui::Widget::TouchEventType type)
 {
     
-    //Close the cocos2d-x game scene and quit the application
-//    Director::getInstance()->end();
-//
-//    #if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
-//    exit(0);
-//#endif
-    
-    /*To navigate back to native iOS screen(if present) without quitting the application  ,do not use Director::getInstance()->end() and exit(0) as given above,instead trigger a custom event created in RootViewController.mm as below*/
-    
-    //EventCustom customEndEvent("game_scene_close_event");
-    //_eventDispatcher->dispatchEvent(&customEndEvent);
-    
-    this->runAction(CCShake::actionWithDuration(0.5f, 1.0f));
-    
+    if (type == cocos2d::ui::Widget::TouchEventType::ENDED)
+    {
+         CCLOG("menuTrueCallback" );
+        if(this->numResult == this->getRealResultNumber()){
+            this->nextRound();
+        }else{
+            this->runAction(CCShake::actionWithDuration(0.5f, 1.0f));
+        }
+    }
+   
+ 
 }
 
 
-void GameScene::menuFalseCallback(Ref* pSender)
+void GameScene::menuFalseCallback(Ref* pSender , cocos2d::ui::Widget::TouchEventType type)
 {
+    if (type == cocos2d::ui::Widget::TouchEventType::ENDED)
+    {
+        if(this->numResult == this->getRealResultNumber()){
+            this->runAction(CCShake::actionWithDuration(0.5f, 1.0f));
+        }else{
+            this->nextRound();
+        }
+    }
     
-    this->runAction(CCShake::actionWithDuration(0.5f, 1.0f));
     
 }
 
+int GameScene::getRandomNumber()
+{
+      auto ran =  rand() % 10;
+    if(ran > 8){
+        return this->getRandomBigNumber();
+    }else{
+        return this->getRandomSmallNumber();
+    }
+}
+
+int GameScene::getRandomBigNumber()
+{
+      return  rand() % 8 + 8;
+}
+
+int GameScene::getRandomSmallNumber()
+{
+    return  rand() % 8 + 1;
+}
+
+int GameScene::getRandomResultNumber(int max)
+{
+    int bound = 14 - max;
+    return  rand() % bound + max;
+}
+
+int GameScene::getRealResultNumber( )
+{
+    return   this->numA +  this->numB;
+}
 
 cocos2d::Color4B GameScene:: getRandomColor()
 {
+    
+    auto ran =  rand() % 8;
+    switch (ran) {
+        case 0:
+            return Color4B::BLUE;
+        case 1:
+            return Color4B::YELLOW;
+        case 2:
+            return Color4B::GREEN;
+        case 3:
+            return Color4B::RED;
+        case 4:
+            return Color4B::MAGENTA;
+        case 5:
+            return Color4B::ORANGE;
+        case 6:
+            return Color4B::GRAY;
+        case 7:
+            return Color4B(200,0,0,255);
+            
+        default:
+             return Color4B::BLUE;
+    }
+    
     
 }
 
--- FreakingMath/Classes/GameScene.h
+++ FreakingMath/Classes/GameScene.h
@@ -3,6 +3,10 @@
 
 #include "cocos2d.h"
 #include "ui/CocosGUI.h"
+#include <stdlib.h>
+#include <string.h>
+#include <sstream>
+#include <ctime>
 
 class GameScene : public cocos2d::Scene
 {
@@ -12,16 +16,27 @@ public:
     virtual bool init();
     
     // a selector callback
-    void menuTrueCallback(cocos2d::Ref* pSender);
-    void menuFalseCallback(cocos2d::Ref* pSender);
+    void menuTrueCallback(cocos2d::Ref* pSender, cocos2d::ui::Widget::TouchEventType type);
+    void menuFalseCallback(cocos2d::Ref* pSender, cocos2d::ui::Widget::TouchEventType type);
     
-    void newGame();
-    cocos2d::Color4B getRandomColor();
+     void newGame();
+     void nextRound();
+     cocos2d::Color4B getRandomColor();
+     int getRandomNumber();
+     int getRandomBigNumber();
+     int getRandomSmallNumber();
+     int getRandomResultNumber(int max);
+     int getRealResultNumber();
     
 protected:
      cocos2d::LayerColor *colorLayer;
      cocos2d::Label *labelRow1;
      cocos2d::Label *labelRow2;
+     int numA;
+     int numB;
+     int numResult;
+     cocos2d::Vec2 row1Pos;
+     cocos2d::Vec2 row2Pos;
     // implement the "static create()" method manually
     CREATE_FUNC(GameScene);
 };
